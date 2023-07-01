9b133763f3a56390d4db7ebb26a68c4861af7d7e
Daniil <dr.chashka@gmail.com>
2023-07-01 03:00:42 +0300

    Убрал лишние зависимости

--- lw6/HttpUrl/HttpUrl.h
+++ lw6/HttpUrl/HttpUrl.h
@@ -1,8 +1,6 @@
 ﻿#pragma once
 #include <string>
 #include <regex>
-#include <sstream>
-#include <cassert>
 
 const unsigned short DEFAULT_HTTP_PORT = 80;
 const unsigned short DEFAULT_HTTPS_PORT = 443;
@@ -28,12 +26,14 @@ public:
 
 private:
     void ParseUrl(const std::string& url);
+
     Protocol ParseProtocol(std::string const& strProtocol);
     unsigned short ParsePort(std::string const& strPort);
     std::string ParseDomain(std::string const& domain);
     std::string ParseDocument(std::string const& document);
     std::string GetStrFromProtocol(Protocol protocol) const;
     unsigned short ProtocolToDefaultPort(Protocol const& protocol) const;
+
     void Initialize(const std::string& domain, const std::string& document, Protocol protocol, unsigned short port);
 
     std::string m_url;
